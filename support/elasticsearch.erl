639656bb1e4ab5976bb537445b9637c8a5489f4f
David de Boer <david@ddeboer.nl>
2014-10-07 10:20:33 +0200

    Improve mapping
    * Unnest language properties, which allows for easier querying:
      use title_en instead of title.en
    * Add nested mapping for blocks
    * Move mapping methods to elasticsearch_mapping

--- support/elasticsearch.erl
+++ support/elasticsearch.erl
@@ -10,8 +10,7 @@
     put_mapping/3,
     put_mapping/4,
     delete_doc/2,
-    delete_doc/3,
-    map_rsc/2
+    delete_doc/3
 ]).
 
 -include("zotonic.hrl").
@@ -28,15 +27,18 @@ index(Context) ->
     SiteName = z_context:site(Context),
     z_convert:to_binary(m_config:get_value(?MODULE, elasticsearch_index, SiteName, Context)).
 
+%% Create Elasticsearch index
 create_index(Index) ->
-    Result = erlastic_search:create_index(z_convert:to_binary(Index)),
-    ?DEBUG(Result).
+    Response = erlastic_search:create_index(z_convert:to_binary(Index)),
+    handle_response(Response).
 
+%% Create Elasticsearch mapping
 put_mapping(Type, Doc, Context) ->
     put_mapping(index(Context), Type, Doc, Context).
 
 put_mapping(Index, Type, Doc, _Context) ->
-    erlastic_search:put_mapping(elasticsearch_connection(), Index, Type, Doc).
+    Response = erlastic_search:put_mapping(elasticsearch_connection(), Index, Type, Doc),
+    handle_response(Response).
 
 %% Save a resource to Elasticsearch
 put_doc(Id, Context) ->
@@ -44,71 +46,26 @@ put_doc(Id, Context) ->
 
 put_doc(Id, Index, Context) ->
     % All resource properties
-    Data = map_rsc(Id, Context)
-        ++ lists:flatten(z_notifier:foldl({elasticsearch_put, Id}, [], Context)),
-
-    % Todo: check if resource is published?
-
-    ?DEBUG(Data),
-
-    erlastic_search:index_doc_with_id(elasticsearch_connection(), Index, "resource", z_convert:to_binary(Id), Data).
+    Props = elasticsearch_mapping:map_rsc(Id, Context),
+    Data = lists:flatten(z_notifier:foldl({elasticsearch_put, Id}, Props, Context)),
+    Response = erlastic_search:index_doc_with_id(
+        elasticsearch_connection(), Index, "resource", z_convert:to_binary(Id), Data
+    ),
+    handle_response(Response).
 
 delete_doc(Id, Context) ->
     delete_doc(Id, index(Context), Context).
 
 delete_doc(Id, Index, Context) ->
     Type = lists:last(m_rsc:is_a(Id, Context)),
-    Result = elastic_search:delete_doc(elasticsearch_connection(), Index, Type, Id),
-    ?DEBUG({"DELETE RESULT", Result}).
+    Response = elastic_search:delete_doc(elasticsearch_connection(), Index, Type, Id),
+    handle_response(Response).
 
-%% Map a Zotonic resource properties
-map_rsc(Id, Context) ->
-    [
-        {category, m_rsc:is_a(Id, Context)},
-        {location, [
-            {lat, m_rsc:p(Id, location_lat, Context)},
-            {lon, m_rsc:p(Id, location_lng, Context)}
-        ]}
-    ] ++
-    lists:foldl(
-        fun(Prop, Acc) ->
-            case map_property(Prop) of
-                undefined ->
-                    Acc;
-                {Key, Value} ->
-                    [{Key, Value}|Acc]
-            end
-        end,
-        [],
-        m_rsc:get(Id, Context)
-    ).
-
-map_property({Key, Value}) ->
-    % Exclude some properties that don't need to be added to index
-    IgnoredProps = [
-        id, pivot_geocode, managed_props, blocks, location_lng, location_lat,
-        location_zoom_levell
-    ],
-    case lists:member(Key, IgnoredProps) of
-        true ->
-            undefined;
-        false ->
-            {Key, map_value(Value)}
+handle_response(Response) ->
+    case Response of
+        {error, {StatusCode, Error}} ->
+            lager:error("Elasticsearch error: ~p: ~p", [StatusCode, Error]),
+            Response;
+        {ok, _} ->
+            Response
     end.
-
-% Make Zotonic resource value Elasticsearch-friendly
-map_value({{Y,M,D},{H,I,S}} = DateTime) when
-is_integer(Y), is_integer(M), is_integer(D), is_integer(H), is_integer(I), is_integer(S) ->
-    z_convert:to_binary(z_convert:to_isotime(DateTime));
-map_value({trans, Translations}) ->
-    lists:map(
-        % Strip HTML tags from translated props. This is only needed for body,
-        % but won't hurt the other translated props.
-        %
-        fun({LangCode, Translation}) ->
-            {LangCode, z_html:strip(Translation)}
-        end,
-        Translations
-    );
-map_value(Value) ->
-    Value.
--- support/elasticsearch_mapping.erl
+++ support/elasticsearch_mapping.erl
@@ -5,46 +5,133 @@
 -include("zotonic.hrl").
 
 -export([
+    map_rsc/2,
     default_mapping/2
 ]).
 
+%% Map a Zotonic resource
+map_rsc(Id, Context) ->
+    [
+        {category, m_rsc:is_a(Id, Context)}
+
+    ] ++
+    map_location(Id, Context) ++
+    lists:foldl(
+        fun(Prop, Acc) ->
+            case map_property(Prop) of
+                undefined ->
+                    Acc;
+                {Key, Value} ->
+                    [{Key, Value}|Acc];
+                Values when is_list(Values) ->
+                    %% Property was split into multiple properties
+                    Values ++ Acc
+            end
+        end,
+        [],
+        m_rsc:get(Id, Context)
+    ).
+
+map_location(Id, Context) ->
+    Lat = m_rsc:p(location_lat, Id, Context),
+    Long = m_rsc:p(location_lng, Id, Context),
+    case z_utils:is_empty(Lat) andalso z_utils:is_empty(Long) of
+        false ->
+            [{location, [
+                {lat, m_rsc:p(Id, location_lat, Context)},
+                {lon, m_rsc:p(Id, location_lng, Context)}
+            ]}];
+        true ->
+            []
+    end.
+
+map_property({Key, Value}) ->
+    % Exclude some properties that don't need to be added to index
+    IgnoredProps = [
+        pivot_geocode, managed_props, blocks, location_lng, location_lat,
+        location_zoom_level
+    ],
+    case lists:member(Key, IgnoredProps) of
+        true ->
+            undefined;
+        false ->
+            case Value of
+                {trans, Translations} ->
+                    lists:map(
+                        fun({LangCode, Translation}) ->
+                            %% Change keys to contain language code: title_en etc.
+                            LangKey = get_language_property(Key, LangCode),
+                            {LangKey, z_html:strip(Translation)}
+                        end,
+                        Translations
+                    );
+                _ ->
+                    {Key, map_value(Value)}
+            end
+    end.
+
+% Make Zotonic resource value Elasticsearch-friendly
+map_value({{Y,M,D},{H,I,S}} = DateTime) when
+is_integer(Y), is_integer(M), is_integer(D), is_integer(H), is_integer(I), is_integer(S) ->
+    z_convert:to_binary(z_convert:to_isotime(DateTime));
+map_value({trans, Translations}) ->
+    lists:map(
+        % Strip HTML tags from translated props. This is only needed for body,
+        % but won't hurt the other translated props.
+        %
+        fun({LangCode, Translation}) ->
+            {LangCode, z_html:strip(Translation)}
+        end,
+        Translations
+    );
+map_value(Value) ->
+    Value.
+
+
 %% Get a default Elasticsearch mapping for Zotonic resources
 default_mapping(Type, Context) ->
-    [{Type, [{
-        properties, [
+    [{Type, [
+        {properties, [
             % location_lat and location_lon are stored in a location property
             {location, [
                 {type, <<"geo_point">>}
+            ]},
+            {blocks, [
+                {type, <<"nested">>}
             ]}
         ]
             ++
             % If mod_translation is enabled, set language-specific analyzers
             case z_module_manager:module_exists(mod_translation) of
                 true ->
-                    lists:map(
-                        fun(Prop) ->
+                    % Translations enabled, so add translated properties
+                    lists:foldl(
+                        fun(Prop, Acc) ->
+                            % For each enabled language, construct an analyzer
                             Langs = lists:map(
                                 fun({LangCode, _}) ->
-                                    {LangCode, [
+                                    {get_language_property(Prop, LangCode), [
                                         {type, string},
                                         {analyzer, get_analyzer(LangCode)}
                                     ]}
                                 end,
                                 m_translation:language_list_enabled(Context)
                             ),
-                            {Prop, [
-                                {type, <<"nested">>},
-                                {properties, Langs}
-                            ]}
+                            Acc ++ Langs
                         end,
+                        [],
                         [title, short_title, body, summary]
                     );
-                false -> []
-            end
-        }]
-    }].
 
-%% Get analyzer for a language
+                false ->
+                    %% Translations not enabled, so add single untranslated property
+                    []
+            end
+        }
+    ]}].
+
+%% Get analyzer for a language, depending on which languages are supported by
+%% Elasticsearch
 get_analyzer(LangCode) ->
     Language =
         string:to_lower(
@@ -55,6 +142,12 @@ get_analyzer(LangCode) ->
         true  -> z_convert:to_binary(Language);
         false -> <<"standard">>
     end.
+
+%% Get property in the form title_en, body_nl etc.
+get_language_property(Prop, Lang) ->
+    z_convert:to_binary(
+        atom_to_list(Prop) ++ "_" ++ atom_to_list(Lang)
+    ).
 
 %% Does Elasticsearch ship an analyzer for the language?
 is_supported_lang(Language) ->
