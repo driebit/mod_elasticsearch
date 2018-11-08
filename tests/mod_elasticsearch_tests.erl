61fdb02009dfaa3a68040c13bd46bf6554e0de99
David de Boer <david@ddeboer.nl>
2018-11-08 14:58:22 +0100

    Test translated property

--- tests/mod_elasticsearch_tests.erl
+++ tests/mod_elasticsearch_tests.erl
@@ -9,13 +9,19 @@ map_test() ->
         [
             {category, text},
             {title, <<"Just a title">>},
-            {empty_string, <<"">>}
+            {empty_string, <<"">>},
+            {translated, {trans, [
+                {nl, <<"Apekool">>},
+                {en, <<"Hogwash">>}
+            ]}}
         ],
         z_acl:sudo(context())
     ),
     Mapped = elasticsearch_mapping:map_rsc(Id, context()),
     ?assertEqual(<<"Just a title">>, maps:get(title, Mapped)),
-    ?assertEqual(null, maps:get(empty_string, Mapped)).
+    ?assertEqual(null, maps:get(empty_string, Mapped)),
+    ?assertEqual(<<"Apekool">>, maps:get(<<"translated_nl">>, Mapped)),
+    ?assertEqual(<<"Hogwash">>, maps:get(<<"translated_en">>, Mapped)).
 
 context() ->
     z_context:new(testsandboxdb).
